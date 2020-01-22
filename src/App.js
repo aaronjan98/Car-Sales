2602b22782b4c4ed45746fd10ae283bbe7b360f7
aaronjan <aaronjan98@gmail.com>
2020-01-22 14:20:05 -0800

    returned an action creator for buyItem & removeFeature

--- src/App.js
+++ src/App.js
@@ -7,31 +7,21 @@ import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
+const buyItem = item => {
+  return {
+    type: 'ADD_FEATURE',
+    payload: item
+  }
+};
+
+const removeFeature = item => {
+  return {
+    type: 'REMOVE_FEATURE',
+    payload: item
+  }
+};
+
 const App = props => {
-  // const state = {
-  //   additionalPrice: 0,
-  //   car: {
-  //     price: 26395,
-  //     name: '2019 Ford Mustang',
-  //     image:
-  //       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-  //     features: []
-  //   },
-  //   additionalFeatures: [
-  //     { id: 1, name: 'V-6 engine', price: 1500 },
-  //     { id: 2, name: 'Racing detail package', price: 1500 },
-  //     { id: 3, name: 'Premium sound system', price: 500 },
-  //     { id: 4, name: 'Rear spoiler', price: 250 }
-  //   ]
-  // };
-
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
 
   return (
     <div className="boxes">
@@ -40,7 +30,7 @@ const App = props => {
         <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} buyItem={props.buyItem}/>
         <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
@@ -55,4 +45,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, {})(App);
+export default connect(mapStateToProps, { buyItem, removeFeature })(App);
---
+++ src/actions/additionalFeatureActions.js
@@ -0,0 +1,7 @@
+
+
+export const addFeature = () => {
+    return {
+        type: 'ADD_FEATURE'
+    }
+}
